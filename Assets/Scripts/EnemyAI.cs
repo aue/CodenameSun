f8ed31f8d95b0f55824ad2ac4a43b95a872d586a
Jonathen Settle <jonathenksettle@ufl.edu>
2016-02-22 22:18:28 -0500

    Initial scene. Enemy patrols and Helper follows player

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float patrolSpeed = 2f;
+    public float patrolWaitTime = 1f;
+    public Transform[] patrolWayPoints;
+
+    private NavMeshAgent nav;
+    //private Transform player;
+    private float patrolTimer;
+    private int wayPointIndex;
+
+	void Awake ()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        //player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+	}
+	
+	void FixedUpdate () {
+        Patrolling();
+	}
+
+    void Patrolling()
+    {
+        //Set an appropriate speed for the NavMeshAgent
+        nav.speed = patrolSpeed;
+
+        if(nav.remainingDistance < nav.stoppingDistance)
+        {
+            //increment timer
+            patrolTimer += Time.deltaTime;
+
+            //if timer exceeds wait time
+            if(patrolTimer >= patrolWaitTime)
+            {
+                //increment waypoint index
+                if(wayPointIndex == patrolWayPoints.Length - 1)
+                {
+                    wayPointIndex = 0;
+                }
+                else
+                {
+                    wayPointIndex++;
+                }
+
+                //reset timer
+                patrolTimer = 0;
+            }
+        }
+
+        else //Use to put back on patrol eventually
+        {
+            
+        }
+
+        nav.destination = patrolWayPoints[wayPointIndex].position;
+    }
+}
---
+++ Assets/Scripts/HelperAI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelperAI : MonoBehaviour
+{
+    public float followSpeed = 2f;
+
+    private NavMeshAgent nav;
+    private Transform player;
+    private Vector3 offset;
+    private Vector3 destination;
+
+    // Use this for initialization
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+    }
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destination = offset + player.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Following();
+    }
+
+    void Following()
+    {
+        nav.speed = followSpeed;
+        nav.destination = destination;
+    }
+}
