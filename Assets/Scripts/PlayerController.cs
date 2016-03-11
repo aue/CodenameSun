5db1b15e781c3c07558a296f467b5a81af573961
Jonathen Settle <jonathenksettle@ufl.edu>
2016-03-11 13:29:44 -0500

    Added ability to jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 2f;
+    public float jumpSpeed = 5f;
 
     private Rigidbody rb;
+    private Transform player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -25,5 +28,10 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown("space"))
+        {
+            player.Translate(Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
+        }
     }
 }
