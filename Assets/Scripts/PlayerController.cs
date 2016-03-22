6900a071ec3d6042a9a9d57e586192e196ec3bba
Jonathen Settle <jonathenksettle@ufl.edu>
2016-03-22 12:22:13 -0400

    Jump works and character is less bouncy.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        //Debug.Log(GameObject.FindGameObjectWithTag(Tags.player).transform.position);
         player = GameObject.FindGameObjectWithTag(Tags.player).transform;
     }
 	
@@ -23,15 +24,19 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        Debug.Log("jump speed is " + jumpSpeed);
+        Debug.Log("player.position is " + player.position);
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3( ((-1) * moveHorizontal), 0.0f, 0.0f);
+        player.position += movement;
 
         if (Input.GetKeyDown("space"))
         {
-            player.Translate(Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
+            //player.Translate(Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
+            player.position += transform.up * jumpSpeed * Time.deltaTime;
         }
     }
 }
